3c31c800a61fad1c6fb8d0d60294677e24662629
Sam <samroth011@gmail.com>
2025-09-13 12:41:17 -0400

    Add styling for grid and etch function
    Use flex-wrap and mouseover to achieve grid which is interactive

--- javascript.js
+++ javascript.js
@@ -1,9 +1,28 @@
 const container = document.querySelector('#container');
 
 const choice = parseInt(prompt('Enter a number',''));
+const boxSize = `${960 / choice}px`;
+console.log(boxSize);
 
-for (let i = 0; i < choice; i++) {
+for (let i = 0; i < 16; i++) {
     const box = document.createElement('div');
     box.classList.toggle('box');
+    box.style.width = boxSize;
+    box.style.height = boxSize;
     container.appendChild(box);
+    box.addEventListener('mouseover', () => {
+    box.classList.add('used')
+});
 }
+
+for (let i = 0; i < (choice * choice); i++) {
+    const box = document.createElement('div');
+    box.classList.toggle('box');
+    box.style.width = boxSize;
+    box.style.height = boxSize;
+    container.appendChild(box);
+    box.addEventListener('mouseover', () => {
+    box.classList.add('used')
+});
+}
+
