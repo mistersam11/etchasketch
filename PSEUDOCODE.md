fcc59c96b771299e6f69f8e8e4f59dbb4bbe4769
Sam <samroth011@gmail.com>
2025-09-13 13:25:26 -0400

    Add resize and clear buttons and logic
    Initialize normal variable which allows clear to maintain grid size

--- PSEUDOCODE.md
+++ PSEUDOCODE.md
@@ -2,3 +2,19 @@
 1. LOOP FOR the number of times specified by the user (i is less than *choice*).
 2. FOR each loop, CREATE a new grid div.
 3. APPEND each div to the parent CONTAINER element.
+
+# Initial load-in
+1. Generate a 16x16 grid using the generation logic
+
+# Button reset
+1. WHEN button is clicked
+2. Clear the grid
+    - using .remove() in a for loop?
+
+# Button resize
+1. WHEN button is clicked
+2. Clear the grid
+    - using .remove() in a for loop?
+3. Prompt user for new number of boxes
+    - IF over 100, alert that number is too high and re-prompt
+    - ELSE save choice and input into grid generation function
--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 const container = document.querySelector('#container');
+let normal = 16;
 
-const choice = parseInt(prompt('Enter a number',''));
-const boxSize = `${960 / choice}px`;
-console.log(boxSize);
-
-for (let i = 0; i < 16; i++) {
+function generateGrid(choice) {
+    for (let i = 0; i < (choice * choice); i++) {
     const box = document.createElement('div');
+    const boxSize = `${960 / choice}px`;
     box.classList.toggle('box');
     box.style.width = boxSize;
     box.style.height = boxSize;
@@ -14,15 +13,36 @@ for (let i = 0; i < 16; i++) {
     box.classList.add('used')
 });
 }
-
-for (let i = 0; i < (choice * choice); i++) {
-    const box = document.createElement('div');
-    box.classList.toggle('box');
-    box.style.width = boxSize;
-    box.style.height = boxSize;
-    container.appendChild(box);
-    box.addEventListener('mouseover', () => {
-    box.classList.add('used')
-});
 }
+
+generateGrid(normal);
+
+function resetGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+}
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    resetGrid();
+    generateGrid(normal);
+});
+
+const resize = document.querySelector('#resize');
+resize.addEventListener('click', () => {
+    resetGrid();
+    choice = parseInt(prompt('Enter a number',''));
+
+    if (choice > 100 || choice < 1) {
+        alert('Choose a number between 1 and 100!')
+    } else {
+        generateGrid(choice);
+        normal = choice;
+    }
+});
+
+    
+
+
 
